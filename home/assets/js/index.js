1dbdd0196e0f44876583a04c407922148d7e8412
MiyueSC <913784771@qq.com>
2024-03-29 16:56:23 +0800

    个人主页

---
+++ home/assets/js/index.js
@@ -0,0 +1,36 @@
+const throttle = function(func, delay = 200) {
+  let timer = null;
+  let startTime = Date.now();
+  return function(...args) {
+    const curTime = Date.now();
+    const remaining = delay - (curTime - startTime);
+    const context = this;
+    clearTimeout(timer);
+    if (remaining <= 0) {
+      func.apply(context, args);
+      startTime = Date.now();
+    } else {
+      timer = setTimeout(func, remaining);
+    }
+  }
+}
+
+let index = 0
+const totalPage = document.querySelectorAll('body > section').length || 0
+const wheelListener = throttle((ev) => {
+  if (!ev) return
+  pageScroll(ev.deltaY > 0 ? 'forward' : 'back')
+})
+function pageScroll(behavior='forward') {
+  if (behavior ==='forward') {
+    if (index === totalPage - 1) return
+    index += 1
+  } else {
+    if (index === 0) return
+    index -= 1
+  }
+  document.body.scrollTo(0, index * window.document.body.clientHeight)
+}
+
+window.addEventListener("wheel", wheelListener)
+
